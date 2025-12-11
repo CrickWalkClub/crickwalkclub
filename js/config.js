d822cb21f7a80ba1fc7ce38843f9900bd7f83b0f
CrickWalkClub <rexonix73@gmail.com>
2025-12-11 21:29:57 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-// Set API base to your backend URL. By default it's localhost.
-export const API_BASE = window.API_BASE || 'http://localhost:3000';
+export const API_BASE = window.API_BASE || 'https://YOUR-BACKEND-URL';
+
